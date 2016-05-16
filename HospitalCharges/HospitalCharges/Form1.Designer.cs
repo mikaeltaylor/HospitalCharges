8755976a90c5c5a9ef046e021a5308e215ea9333
mikaeltaylor <mikael.e.taylor@gmail.com>
2016-05-16 07:49:13 -0500

    Hospital Charges commit

---
+++ HospitalCharges/HospitalCharges/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HospitalCharges
+{
+    partial class HospitalCharges
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDays = new System.Windows.Forms.Label();
+            this.txtDaysInput = new System.Windows.Forms.TextBox();
+            this.lblMedCharges = new System.Windows.Forms.Label();
+            this.txtMedInput = new System.Windows.Forms.TextBox();
+            this.lblSurgicalCharges = new System.Windows.Forms.Label();
+            this.txtSurgicalInput = new System.Windows.Forms.TextBox();
+            this.lblRehabCharges = new System.Windows.Forms.Label();
+            this.txtRehabInput = new System.Windows.Forms.TextBox();
+            this.lblLabFees = new System.Windows.Forms.Label();
+            this.txtLabInput = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtDateInput = new System.Windows.Forms.TextBox();
+            this.txtTimeInput = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Location = new System.Drawing.Point(57, 31);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(180, 13);
+            this.lblDays.TabIndex = 0;
+            this.lblDays.Text = "How many days did the patient stay?";
+            // 
+            // txtDaysInput
+            // 
+            this.txtDaysInput.Location = new System.Drawing.Point(244, 28);
+            this.txtDaysInput.Name = "txtDaysInput";
+            this.txtDaysInput.Size = new System.Drawing.Size(100, 20);
+            this.txtDaysInput.TabIndex = 1;
+            this.txtDaysInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDaysInput_KeyPress);
+            // 
+            // lblMedCharges
+            // 
+            this.lblMedCharges.AutoSize = true;
+            this.lblMedCharges.Location = new System.Drawing.Point(74, 61);
+            this.lblMedCharges.Name = "lblMedCharges";
+            this.lblMedCharges.Size = new System.Drawing.Size(163, 13);
+            this.lblMedCharges.TabIndex = 2;
+            this.lblMedCharges.Text = "What were the medical charges?";
+            // 
+            // txtMedInput
+            // 
+            this.txtMedInput.Location = new System.Drawing.Point(244, 58);
+            this.txtMedInput.Name = "txtMedInput";
+            this.txtMedInput.Size = new System.Drawing.Size(100, 20);
+            this.txtMedInput.TabIndex = 3;
+            this.txtMedInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMedInput_KeyPress);
+            // 
+            // lblSurgicalCharges
+            // 
+            this.lblSurgicalCharges.AutoSize = true;
+            this.lblSurgicalCharges.Location = new System.Drawing.Point(74, 88);
+            this.lblSurgicalCharges.Name = "lblSurgicalCharges";
+            this.lblSurgicalCharges.Size = new System.Drawing.Size(163, 13);
+            this.lblSurgicalCharges.TabIndex = 4;
+            this.lblSurgicalCharges.Text = "What were the surgical charges?";
+            // 
+            // txtSurgicalInput
+            // 
+            this.txtSurgicalInput.Location = new System.Drawing.Point(244, 85);
+            this.txtSurgicalInput.Name = "txtSurgicalInput";
+            this.txtSurgicalInput.Size = new System.Drawing.Size(100, 20);
+            this.txtSurgicalInput.TabIndex = 5;
+            this.txtSurgicalInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSurgicalInput_KeyPress);
+            // 
+            // lblRehabCharges
+            // 
+            this.lblRehabCharges.AutoSize = true;
+            this.lblRehabCharges.Location = new System.Drawing.Point(11, 114);
+            this.lblRehabCharges.Name = "lblRehabCharges";
+            this.lblRehabCharges.Size = new System.Drawing.Size(227, 13);
+            this.lblRehabCharges.TabIndex = 6;
+            this.lblRehabCharges.Text = "What were the physical rehabilitation charges?";
+            // 
+            // txtRehabInput
+            // 
+            this.txtRehabInput.Location = new System.Drawing.Point(244, 111);
+            this.txtRehabInput.Name = "txtRehabInput";
+            this.txtRehabInput.Size = new System.Drawing.Size(100, 20);
+            this.txtRehabInput.TabIndex = 7;
+            this.txtRehabInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtRehabInput_KeyPress);
+            // 
+            // lblLabFees
+            // 
+            this.lblLabFees.AutoSize = true;
+            this.lblLabFees.Location = new System.Drawing.Point(115, 144);
+            this.lblLabFees.Name = "lblLabFees";
+            this.lblLabFees.Size = new System.Drawing.Size(123, 13);
+            this.lblLabFees.TabIndex = 8;
+            this.lblLabFees.Text = "What were the lab fees?";
+            // 
+            // txtLabInput
+            // 
+            this.txtLabInput.Location = new System.Drawing.Point(243, 141);
+            this.txtLabInput.Name = "txtLabInput";
+            this.txtLabInput.Size = new System.Drawing.Size(100, 20);
+            this.txtLabInput.TabIndex = 9;
+            this.txtLabInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLabInput_KeyPress);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(205, 171);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(33, 13);
+            this.lblDate.TabIndex = 10;
+            this.lblDate.Text = "Date:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(204, 198);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(33, 13);
+            this.lblTime.TabIndex = 11;
+            this.lblTime.Text = "Time:";
+            // 
+            // txtDateInput
+            // 
+            this.txtDateInput.Location = new System.Drawing.Point(243, 168);
+            this.txtDateInput.Name = "txtDateInput";
+            this.txtDateInput.Size = new System.Drawing.Size(100, 20);
+            this.txtDateInput.TabIndex = 12;
+            // 
+            // txtTimeInput
+            // 
+            this.txtTimeInput.Location = new System.Drawing.Point(244, 195);
+            this.txtTimeInput.Name = "txtTimeInput";
+            this.txtTimeInput.Size = new System.Drawing.Size(100, 20);
+            this.txtTimeInput.TabIndex = 13;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(194, 226);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 14;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // HospitalCharges
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 261);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtTimeInput);
+            this.Controls.Add(this.txtDateInput);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.txtLabInput);
+            this.Controls.Add(this.lblLabFees);
+            this.Controls.Add(this.txtRehabInput);
+            this.Controls.Add(this.lblRehabCharges);
+            this.Controls.Add(this.txtSurgicalInput);
+            this.Controls.Add(this.lblSurgicalCharges);
+            this.Controls.Add(this.txtMedInput);
+            this.Controls.Add(this.lblMedCharges);
+            this.Controls.Add(this.txtDaysInput);
+            this.Controls.Add(this.lblDays);
+            this.Name = "HospitalCharges";
+            this.Text = "Hospital Charges v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.TextBox txtDaysInput;
+        private System.Windows.Forms.Label lblMedCharges;
+        private System.Windows.Forms.TextBox txtMedInput;
+        private System.Windows.Forms.Label lblSurgicalCharges;
+        private System.Windows.Forms.TextBox txtSurgicalInput;
+        private System.Windows.Forms.Label lblRehabCharges;
+        private System.Windows.Forms.TextBox txtRehabInput;
+        private System.Windows.Forms.Label lblLabFees;
+        private System.Windows.Forms.TextBox txtLabInput;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtDateInput;
+        private System.Windows.Forms.TextBox txtTimeInput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ HospitalCharges/HospitalCharges/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Program    :   Hospital Charges
+// Developer  :   Mikael Taylor
+// Date       :   3/4/2016
+// Description:   This program allows a user to enter various hospital charges such as
+//                days stayed, medication charge, surgical charge, rehabilitation charge,
+//                and calculates the fees for each category(stay, and misc) before writing
+//                them to a text file with a default name of charges.txt.  It also allows the
+//                user to enter a time and date, otherwise it populates that data when the form
+//                is initialized and allows the user to simply keep the time/date at it's default
+//                setting. It's methods are modular, and only called on clicking the submit button.
+//                It also utilizes keypress validation, which has proven superior to the other
+//                methods of validation thus far, for each textbox aside from date/time textboxes.
+namespace HospitalCharges
+{
+    public partial class HospitalCharges : Form
+    {
+        private double dayCharge;//Daily base charge
+        private double medCharge;//Medication charges
+        private double surgCharge;//Surgical charges
+        private double labCharge;//Lab Fees
+        private double rehabCharge;//Physical rehabilitation charges
+        private double days;//Days stayed
+        private double stayCharges;//Stay charges
+        private double miscCharges;//Misc charges
+        private double totalCharges;//Total charges
+        public HospitalCharges()
+        {
+            InitializeComponent();
+            this.txtDateInput.Text = DateTime.Today.ToString("MM/dd/yyyy");
+            //Set the default date
+            this.txtTimeInput.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            //Set the default time
+        }
+        public void CalcStayCharges()
+        {//We'll use CalcStayCharges to give us the stay charges based on number of days stayed multiplied by the dayCharge.
+            dayCharge = 350;
+            //dayCharge is the $350 a day that is the daily base charge.
+            days = Double.Parse(txtDaysInput.Text);
+            //Number of days set by user input. We'll parse it here.
+            stayCharges = dayCharge * days;
+            //Stay Charges are simply the dayCharge multiplied by days stayed.
+        }
+        public void CalcMiscCharges()
+        {//We'll use CalcMiscCharges to calculate the total misc charges provided by the user.
+            medCharge = double.Parse(txtMedInput.Text);
+            //Medication charges provided by user input.
+            surgCharge = double.Parse(txtSurgicalInput.Text);
+            //Surgical charges provided by user input.
+            labCharge = double.Parse(txtLabInput.Text);
+            //Lab fees provided by user input.
+            rehabCharge = double.Parse(txtRehabInput.Text);
+            //Physical rehabilitation charges provided by user input.
+            miscCharges = medCharge + surgCharge + labCharge + rehabCharge;
+            //The sum of all these charges is the misc charges.
+        }
+        public void CalcTotalCharges()
+        {//CalcTotalCharges calculates the total charges(Stay charges, and misc charges)
+            totalCharges = stayCharges + miscCharges;
+            //Total charges incurred.
+        }
+        public void writeFile()
+        {
+            if (txtDateInput.Text != null && txtTimeInput.Text != null && txtSurgicalInput.Text != null && txtRehabInput.Text != null && txtMedInput.Text != null && txtLabInput.Text != null && txtDaysInput.Text != null)
+            {//If the textboxes aren't empty..
+                try
+                {//Try to..
+                    DateTime dateTest;//Test the date format
+                    if (DateTime.TryParseExact(txtDateInput.Text, "MM/dd/yyy", null, System.Globalization.DateTimeStyles.None, out dateTest) == false)
+                    {
+                        MessageBox.Show("Incorrect Date Entry.  Try again with valid MM/DD/YYYY input, or leave at default.");
+                        this.txtDateInput.Text = DateTime.Today.ToString("MM/dd/yyyy");
+                        //Set back to the default date  
+                    }
+                    DateTime timeTest;//Test the time format
+                    if (DateTime.TryParseExact(txtTimeInput.Text, "hh:mm:ss tt", null, System.Globalization.DateTimeStyles.None, out timeTest) == false)
+                    {
+                        MessageBox.Show("Incorrect Time Entry.  Try again with valid hh:mm:ss AM/PM input, or leave at default");
+                        this.txtTimeInput.Text = DateTime.Now.ToString("hh:mm:ss tt");
+                        //Set back to the default time          
+                    }
+
+                    //Bring in user input for the quantity to be written.
+                    saveFileDialog.FileName = "charges.txt";
+                    saveFileDialog.Filter = "Text Files(TXT)|*.TXT";
+                    saveFileDialog.ShowDialog();
+                    //Ask the user where to save the file.
+                    string file = saveFileDialog.FileName;
+                    //Put the filename in a variable so that we can use it.
+                    StreamWriter writer = new StreamWriter(file);
+                    //Use StreamWriter so that we can write to the file.
+                    writer.WriteLine(txtDateInput.Text);//Write the correct text to the file
+                    writer.WriteLine(txtTimeInput.Text);//Write the correct text to the file
+                    writer.WriteLine("Stay Charges : " + stayCharges);//Write the correct text to the file
+                    writer.WriteLine("Misc Charges : " + miscCharges);//Write the correct text to the file
+                    writer.WriteLine("Total Charges: " + totalCharges);//Write the correct text to the file
+                    writer.Close();//Close the stream writer
+                    //Close the streamwriter
+                }//end try
+                catch (Exception ex)
+                {//If an exception occurs..
+                    MessageBox.Show("Error Code: " + ex);
+                    //Tell me what happened.
+                }//close catch
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid data in ALL of the text boxes.");
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {//When the user clicks the submit button..
+            CalcStayCharges();
+            //Calculate the stay charges
+            CalcMiscCharges();
+            //Calculate the misc charges
+            CalcTotalCharges();
+            //Calculate the total charges
+            writeFile();
+            //Write the file to charges.txt
+        }
+        //Keypress validation for each textbox is below.
+        private void txtDaysInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                MessageBox.Show("You will need to enter a correct number of days stayed.");
+            }
+        }
+
+        private void txtMedInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                MessageBox.Show("You will need to enter a correct amount of medication charges in decimal format without the currency symbol($).");
+            }
+
+        }
+
+        private void txtSurgicalInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                MessageBox.Show("You will need to enter a correct amount of surgical charges in decimal format without the currency symbol($).");
+            }
+        }
+
+        private void txtRehabInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                MessageBox.Show("You will need to enter a correct amount of physical rehabilitation charges in decimal format without the currency symbol($).");
+            }
+        }
+
+        private void txtLabInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                MessageBox.Show("You will need to enter a correct amount of lab fee in decimal format without the currency symbol($).");
+            }
+        }
+
+
+
+    }
+
+}
